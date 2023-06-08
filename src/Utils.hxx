bb70a634c1de4b4a70f0987ddd400226a353226f
Coder. Leo Lee <74029782+leo4048111@users.noreply.github.com>
2023-06-08 15:03:20 +0800

    Create Utils.hxx

---
+++ src/Utils.hxx
@@ -0,0 +1,18 @@
+#include <string>
+
+namespace fpd
+{
+    class Utils
+    {
+        Utils() = delete;
+        Utils(Utils &&) = delete;
+        Utils(const Utils &) = delete;
+
+    public:
+        static std::string getFilenameNoExt(const std::string_view &filename)
+        {
+            size_t lastindex = filename.find_last_of(".");
+            return std::string(filename.substr(0, lastindex));
+        }
+    };
+}
