2b7698cc4be09fca1daf5c006173d14534253a9b
Coder. Leo Lee <74029782+leo4048111@users.noreply.github.com>
2023-06-08 17:29:12 +0800

    Update Player.cc

--- src/Player.cc
+++ src/Player.cc
@@ -80,54 +80,80 @@ namespace fpd
         else
             audioStreamidx = ec;
 
+        AVStream *videoInStream = avFormatCtx->streams[videoStreamidx];
+        AVStream *audioInStream = avFormatCtx->streams[audioStreamidx];
+        const AVCodec *videoInCodec = avcodec_find_decoder(videoInStream->codecpar->codec_id);
+        const AVCodec *audioInCodec = avcodec_find_decoder(audioInStream->codecpar->codec_id);
+        AVCodecContext *videoInCodecCtx = avcodec_alloc_context3(videoInCodec);
+        AVCodecContext *audioInCodecCtx = avcodec_alloc_context3(audioInCodec);
+
         // Get pointer to codec parameters
-        AVCodecParameters *videoCodecParams = avFormatCtx->streams[videoStreamidx]->codecpar;
-        AVCodecParameters *audioCodecParams = avFormatCtx->streams[audioStreamidx]->codecpar;
+        AVCodecParameters *videoCodecParams = videoInStream->codecpar;
+        AVCodecParameters *audioCodecParams = audioInStream->codecpar;
+
+        if ((ec = avcodec_parameters_to_context(videoInCodecCtx, videoCodecParams)) < 0)
+        {
+            LOG_ERROR("Failed to copy video codec parameters to input codec context in file: %s", file.data());
+            return ec;
+        }
+
+        if ((ec = avcodec_parameters_to_context(audioInCodecCtx, audioCodecParams)) < 0)
+        {
+            LOG_ERROR("Failed to copy audio codec parameters to input codec context in file: %s", file.data());
+            return ec;
+        }
+
+        if ((ec = avcodec_open2(videoInCodecCtx, videoInCodec, nullptr)) < 0)
+        {
+            LOG_ERROR("Failed to open video codec in file: %s", file.data());
+            return ec;
+        }
+
+        if ((ec = avcodec_open2(audioInCodecCtx, audioInCodec, nullptr)) < 0)
+        {
+            LOG_ERROR("Failed to open audio codec in file: %s", file.data());
+            return ec;
+        }
 
         auto filenameNoExt = Utils::getFilenameNoExt(file);
         auto videoFilename = filenameNoExt + ".h265";
         auto audioFilename = filenameNoExt + ".aac";
 
-        AVFormatContext *videoOutFormatCtx = avformat_alloc_context();
-        AVFormatContext *audioOutFormatCtx = avformat_alloc_context();
+        AVFormatContext *videoOutFormatCtx, *audioOutFormatCtx;
+        if ((ec = avformat_alloc_output_context2(&videoOutFormatCtx, nullptr, nullptr, videoFilename.c_str())))
+        {
+            LOG_ERROR("Failed to allocate output context for video file: %s", videoFilename.c_str());
+            return ec;
+        }
 
-        const AVOutputFormat *videoOutFormat = av_guess_format(nullptr, videoFilename.c_str(), nullptr);
-        const AVOutputFormat *audioOutFormat = av_guess_format(nullptr, audioFilename.c_str(), nullptr);
-
-        videoOutFormatCtx->oformat = videoOutFormat;
-        audioOutFormatCtx->oformat = audioOutFormat;
+        if ((avformat_alloc_output_context2(&audioOutFormatCtx, nullptr, nullptr, audioFilename.c_str())))
+        {
+            LOG_ERROR("Failed to allocate output context for audio file: %s", audioFilename.c_str());
+            return ec;
+        }
 
         AVStream *videoOutStream = avformat_new_stream(videoOutFormatCtx, nullptr);
         AVStream *audioOutStream = avformat_new_stream(audioOutFormatCtx, nullptr);
 
-        if ((ec = avcodec_parameters_copy(videoOutStream->codecpar, videoCodecParams)) < 0)
-        {
-            LOG_ERROR("Failed to copy video codec parameters to output stream in file: %s", file.data());
-            return ec;
-        }
+        const AVOutputFormat *videoOutFormat = av_guess_format(nullptr, videoFilename.c_str(), nullptr);
+        const AVOutputFormat *audioOutFormat = av_guess_format(nullptr, audioFilename.c_str(), nullptr);
 
-        if ((ec = avcodec_parameters_copy(audioOutStream->codecpar, audioCodecParams)) < 0)
-        {
-            LOG_ERROR("Failed to copy audio codec parameters to output stream in file: %s", file.data());
-            return ec;
-        }
+        const AVCodec *videoOutCodec = avcodec_find_encoder(videoOutFormat->video_codec);
+        const AVCodec *audioOutCodec = avcodec_find_encoder(audioOutFormat->audio_codec);
 
-        // videoOutStream->codecpar->codec_tag = 0;
-        // audioOutStream->codecpar->codec_tag = 0;
+        AVCodecContext *videoOutCodecCtx = avcodec_alloc_context3(videoOutCodec);
+        AVCodecContext *audioOutCodecCtx = avcodec_alloc_context3(audioOutCodec);
+        videoOutCodecCtx->width = videoInCodecCtx->width;
+        videoOutCodecCtx->height = videoInCodecCtx->height;
+        videoOutCodecCtx->pix_fmt = videoOutCodec->pix_fmts[0];
+        videoOutCodecCtx->time_base = videoInStream->time_base;
+        avcodec_open2(videoOutCodecCtx, videoOutCodec, nullptr);
+        avcodec_open2(audioOutCodecCtx, audioOutCodec, nullptr);
 
-        if ((ec = avio_open(&videoOutFormatCtx->pb, videoFilename.c_str(), AVIO_FLAG_WRITE)) < 0)
-        {
-            LOG_ERROR("Failed to open video output file: %s", videoFilename.c_str());
-            return ec;
-        }
-
-        if ((ec = avio_open(&audioOutFormatCtx->pb, audioFilename.c_str(), AVIO_FLAG_WRITE)) < 0)
-        {
-            LOG_ERROR("Failed to open audio output file: %s", audioFilename.c_str());
-            return ec;
-        }
-
-        AVPacket pkt;
+        avcodec_parameters_from_context(videoOutStream->codecpar, videoOutCodecCtx);
+        videoOutStream->time_base = videoOutCodecCtx->time_base;
+        avcodec_parameters_from_context(audioOutStream->codecpar, audioOutCodecCtx);
+        audioOutStream->time_base = audioOutCodecCtx->time_base;
 
         if ((ec = avformat_write_header(videoOutFormatCtx, nullptr)) < 0)
         {
@@ -141,15 +167,30 @@ namespace fpd
         //     return ec;
         // }
 
+        AVPacket pkt;
+        AVFrame *frame = av_frame_alloc();
+        SwsContext *swsCtx = sws_getContext(videoInCodecCtx->width, videoInCodecCtx->height, videoInCodecCtx->pix_fmt, videoOutCodecCtx->width, videoOutCodecCtx->height, videoOutCodecCtx->pix_fmt, SWS_BILINEAR, nullptr, nullptr, nullptr);
+
         while (av_read_frame(avFormatCtx, &pkt) >= 0)
         {
             if (pkt.stream_index == videoStreamidx)
             {
-                pkt.pts = av_rescale_q_rnd(pkt.pts, avFormatCtx->streams[videoStreamidx]->time_base, videoOutStream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-                pkt.dts = av_rescale_q_rnd(pkt.dts, avFormatCtx->streams[videoStreamidx]->time_base, videoOutStream->time_base, (AVRounding)(AV_ROUND_NEAR_INF | AV_ROUND_PASS_MINMAX));
-                pkt.duration = av_rescale_q(pkt.duration, avFormatCtx->streams[videoStreamidx]->time_base, videoOutStream->time_base);
-                pkt.pos = -1;
+                avcodec_send_packet(videoInCodecCtx, &pkt);
+                avcodec_receive_frame(videoInCodecCtx, frame);
+
+                AVFrame *outFrame = av_frame_alloc();
+                outFrame->format = videoOutCodecCtx->pix_fmt;
+                outFrame->width = videoInCodecCtx->width;
+                outFrame->height = videoInCodecCtx->height;
+                av_frame_get_buffer(outFrame, 0);
+                sws_scale(swsCtx, frame->data, frame->linesize, 0, videoInCodecCtx->height, outFrame->data, outFrame->linesize);
+
+                avcodec_send_frame(videoOutCodecCtx, outFrame);
+                avcodec_receive_packet(videoOutCodecCtx, &pkt);
+
+                pkt.stream_index = videoOutStream->index;
                 av_interleaved_write_frame(videoOutFormatCtx, &pkt);
+                av_frame_free(&outFrame);
             }
             // else if(pkt.stream_index == audioStreamidx)
             // {
@@ -168,10 +209,18 @@ namespace fpd
 
         LOG_INFO("Dumped video stream to file: %s", videoFilename.c_str());
         LOG_INFO("Dumped audio stream to file: %s", audioFilename.c_str());
+        
+        av_frame_free(&frame);
+        sws_freeContext(swsCtx);
+        avcodec_close(videoInCodecCtx);
+        avcodec_close(audioInCodecCtx);
+        avcodec_close(videoOutCodecCtx);
+        avcodec_close(audioOutCodecCtx);
 
         avformat_close_input(&avFormatCtx);
-        avio_close(videoOutFormatCtx->pb);
-        avio_close(audioOutFormatCtx->pb);
+        
+        avformat_free_context(videoOutFormatCtx);
+        avformat_free_context(audioOutFormatCtx);
         avformat_free_context(avFormatCtx);
 
         return ec;
